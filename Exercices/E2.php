6d56275013aa6634ae64845e80d82a24ad5c6f5a
EC2 Default User <ec2-user@ip-172-31-1-59.eu-west-1.compute.internal>
2019-03-07 07:57:03 +0000

    E2

---
+++ Exercices/E2.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function exercice2($var) {
+    $a = "Dans le port d’Amsterdam";
+    $b = "Y a des marins qui mangent";
+    $c = "Sur des nappes trop blanches";
+    
+    // PART 1 : On écrit le contenu de E2.txt suivi de $a, b ou c (en fonction de var) 
+    // dans E2.$var.txt
+    
+    
+}
+
+exercice2("a");
+exercice2("b");
+exercice2("c");
+
+
+// PART 3 : Ajouter un long lorem ipsum sans interpolation ici
+$longTexte = '';
+
+// PART 4 Ecrire la fonction remplacerLorem, qui remplace le mot Lorem par 'ornithorynque'
+// dans longTexte, mais qui ne renvoie rien (elle modifie)
+
+
+var_dump($longTexte);
